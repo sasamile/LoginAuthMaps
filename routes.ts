3d27a8225c081bff73f9a0ceda672d21fcfba07e
Santiago Suescun <116961553+sasamile@users.noreply.github.com>
2024-11-05 10:17:46 -0500

    Boton de password

--- routes.ts
+++ routes.ts
@@ -2,7 +2,7 @@ export const DEFAULT_LOGIN_REDIRECT_USER = "/dashboard";
 export const DEFAULT_LOGIN_REDIRECT_ADMIN = "/admin";
 export const DEFAULT_AUTH_REDIRECT = "/sign-in";
 
-export const publicRoutes = ["/", "/api/auth/**", "/api/uploadthing" , "/sign-up"];
+export const publicRoutes = ["/", "/api/auth/**", "/api/uploadthing" , "/sign-up", "/password" ,"/password-verify"];
 export const authRoutes = ["/sign-in"];
 
 export const apiAuthPrefix = "/api/auth";
